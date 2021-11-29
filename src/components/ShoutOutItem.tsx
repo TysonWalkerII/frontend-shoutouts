8c7fb2a62dd944ec54ce17c04030c3357ae6765b
Tyson <tywalk10@gmail.com>
2021-11-29 10:18:46 -0500

    fixed

--- src/components/ShoutOutItem.tsx
+++ src/components/ShoutOutItem.tsx
@@ -1,7 +1,5 @@
-import { userInfo } from "os";
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import AuthContext from "../context/AuthContext";
+
 import ShoutOut from "../models/ShoutOut";
 import "./ShoutOutItem.css";
 
